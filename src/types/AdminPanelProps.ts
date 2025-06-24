121345ada5d629a29fe8551176dd7c8c047cb3ec
HishamAbualhaj <hishamecommerce@gmail.com>
2025-06-24 21:51:52 +0300

    Create reuseable component to handle action buttons in table , add admin panel props

---
+++ src/types/AdminPanelProps.ts
@@ -0,0 +1,23 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import { tableProps } from "./TableTypes";
+
+export interface adminPanelProps extends tableProps {
+  panelTitle: string;
+  btnText?: string;
+  filterContent?: (
+    handleChange: (
+      e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => void,
+    isChange: boolean
+  ) => ReactNode;
+  filterAction?: () => void;
+  mainPopup?: PopupProps;
+  tablePopup?: PopupProps[];
+}
+
+export interface PopupProps {
+  popupTitle?: string;
+  popupContent?: { text: string; item: ReactNode; data?: string }[] | string;
+  popupActionText?: string;
+  popupAction?: () => void;
+}
