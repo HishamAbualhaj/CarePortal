fb699c218766d260ec87b03e7dcc3bd7bc1d7008
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-07-07 11:00:58 +0300

    useUpload hook now can delete image

--- src/hooks/useUpload.ts
+++ src/hooks/useUpload.ts
@@ -1,15 +1,34 @@
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import { storage } from "@/firebase/config";
 
-async function useUpload(file: File | undefined): Promise<string | undefined> {
-  if (!file) return;
+async function useUpload(
+  file: File | null | string,
+  type: string = "upload"
+): Promise<string | null> {
+  if (!file) return null;
 
-  const storageRef = ref(storage, `images/${file.name}`);
+  let storageRef = null;
+  if (typeof file === "string") {
+    storageRef = ref(storage, `images/${file}`);
+  } else {
+    storageRef = ref(storage, `images/${file.name}`);
+  }
 
-  await uploadBytes(storageRef, file);
+  if (type === "upload" && typeof file !== "string") {
+    await uploadBytes(storageRef, file);
 
-  const url = await getDownloadURL(storageRef);
-  return url;
+    const url = await getDownloadURL(storageRef);
+    return url;
+  }
+
+  await deleteObject(storageRef);
+
+  return "Image deleted";
 }
 
 export default useUpload;
