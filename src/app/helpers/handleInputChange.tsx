8a12a9d1773bd7314e3072bbffa58c4f66a95de1
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-07-10 16:04:23 +0300

    change some data to be optional

--- src/app/helpers/handleInputChange.tsx
+++ src/app/helpers/handleInputChange.tsx
@@ -5,17 +5,17 @@ export const handleChange = <T,>(
     HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
   >,
   setData: Dispatch<SetStateAction<T | Record<string, any>>>,
-  setFile: Dispatch<SetStateAction<File | null>>,
-  setTempFileUrl: Dispatch<SetStateAction<string | null>>
+  setFile?: Dispatch<SetStateAction<File | null>>,
+  setTempFileUrl?: Dispatch<SetStateAction<string | null>>
 ) => {
   const { id, value, type } = e.target;
 
   if (type === "file" && e.target instanceof HTMLInputElement) {
     const currentFile = e.target.files?.[0] ?? null;
-    setFile(currentFile);
+    setFile?.(currentFile);
     if (currentFile) {
       const tempUrl = URL.createObjectURL(currentFile);
-      setTempFileUrl(tempUrl);
+      setTempFileUrl?.(tempUrl);
 
       setData((prev: Record<string, any>) => ({
         ...prev,
