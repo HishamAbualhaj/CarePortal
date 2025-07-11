9317f27e717cf84019e155dc8f7cb7b65cc275ac
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-07-11 14:43:40 +0300

    Get messages for admin dashboard

---
+++ src/app/api/getMessages/route.ts
@@ -0,0 +1,27 @@
+import { adminDB } from "@/firebase/adminConfig";
+import { NextResponse, NextRequest } from "next/server";
+import withAuth from "@/lib/withAuth";
+export async function GET(req: NextRequest) {
+  try {
+    return withAuth(req, async (user, req) => {
+        if (user.role !== "admin") {
+          return NextResponse.json(
+            {
+              status: false,
+              msg: "Authentication credentials are wrong",
+            },
+            { status: 401 }
+          );
+        }
+      const snapshot = await adminDB.collection("messages").get();
+      const messageData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      return NextResponse.json({ status: true, msg: messageData });
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ status: false, msg: "Something went wrong" });
+  }
+}
