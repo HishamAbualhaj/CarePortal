35647a841c65ffefd922bc55d207b89d9210fe3d
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-07-17 19:43:04 +0300

    Create api route to set token to cookies or unset, return userData after setting

--- src/app/api/setToken/route.ts
+++ src/app/api/setToken/route.ts
@@ -1,10 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { adminAuth } from "@/firebase/adminConfig";
+import { adminAuth, adminDB } from "@/firebase/adminConfig";
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
+    let tokenAge = 60 * 60 * 24 * 7; // 7 days
+    const userData = await adminAuth.verifyIdToken(body?.token);
+    if (body?.type === "unset") {
+      tokenAge = 0;
+      await adminDB
+        .collection(`${userData?.role === "doctor" ? "doctors" : "users"}`)
+        .doc(userData.uid)
+        .update({
+          status: false,
+        });
+    } else {
+      await adminDB
+        .collection(`${userData?.role === "doctor" ? "doctors" : "users"}`)
+        .doc(userData.uid)
+        .update({
+          status: true,
+        });
+    }
     await adminAuth.verifyIdToken(body?.token);
 
     (await cookies()).set({
@@ -13,11 +31,15 @@ export async function POST(req: NextRequest) {
       httpOnly: true,
       path: "/",
       secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
+      maxAge: tokenAge,
     });
 
-    return NextResponse.json({ status: "success" });
+    return NextResponse.json({ status: "success", msg: userData });
   } catch (error) {
-    return NextResponse.json({ status: "Invalid token" }, { status: 500 });
+    console.error(error);
+    return NextResponse.json(
+      { status: "Invalid token", msg: "Can't set token" },
+      { status: 500 }
+    );
   }
 }
